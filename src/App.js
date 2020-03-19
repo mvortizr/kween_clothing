211ea9622fa4ea7f1aa46ea4e28543602834df86
María V. Ortiz <mariatua2@gmail.com>
2020-03-19 10:41:39 -0400

    Menu directory

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import HomePage from './pages/homepage/Homepage';
 
 function App() {
   return (
     <div>
+    	<HomePage/>
     </div>
   );
 }
