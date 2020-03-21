17cd027ecd8de7f0b3751ae3c719835155f63451
María V. Ortiz <mariatua2@gmail.com>
2020-03-21 19:00:51 -0400

    collection preview, shop page

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,45 +1,15 @@
 import React, {useState} from 'react'
+import CollectionPreview from '../../components/collection-preview/CollectionPreview'
+import SHOP_DATA from './shop-data.js';
 
-const info = [
-    {
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-      id: 1,
-      linkUrl:'hats'
-    },
-    {
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-      id: 2,
-      linkUrl:'jackets'
-    },
-    {
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-      id: 3,
-      linkUrl:'sneakers'
-    },
-    {
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-      size: 'large',
-      id: 4,
-      linkUrl:'womens'
-    },
-    {
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-      size: 'large',
-      id: 5,
-      linkUrl:'mens'
-    }
-  ];
 const Shop = (props) => {
-    let [collections, setCollection] = useState(info);
+    let [collections, setCollection] = useState(SHOP_DATA);
     return (
-        <p>SHOP NOW
-            {console.log(props)}
-        </p>
+      <div className="shop-page">
+        {
+          collections.map(({ id, ...collectionInfo }) => (<CollectionPreview key={id} {...collectionInfo} />))
+        }
+      </div>
     );
 }
 
