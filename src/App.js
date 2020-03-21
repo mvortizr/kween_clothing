d039128dc558cfba9cc8f22e87c4017b6c112bce
María V. Ortiz <mariatua2@gmail.com>
2020-03-21 18:26:10 -0400

    iterating over object

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import './App.css';
-import {Switch, Route} from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 import HomePage from './pages/homepage/Homepage';
+import Shop from './pages/shop/Shop';
+
+let routes = [
+  {
+    path: '/',
+    render: (props) => <HomePage {...props}/>
+  },
+  {
+    path: '/shop',
+    render: (props) => <Shop {...props} />
+  }
+];
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' render={ ()=> <HomePage />}/>
-    </Switch>
+  <div>
+    {routes.map(
+        ({path,render}) => (
+          <Route exact path={path} render={(props) => render(props)}/>
+        )
+      )
+    }
+  </div>
   );
 }
 
